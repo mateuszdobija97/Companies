a4d880620b18948efa7526f1496758f7817f0112
Mateusz Dobija <mateusz.dobija.rce@gmail.com>
2020-05-25 21:58:44 +0200

    add filter input

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ListItems from '../components/ListItems';
+import { FaBeer } from 'react-icons/fa';
 
 const Main = () => {
 
     const [companies, setCompanies] = useState([]);
+    const [search, setSearch] = useState('');
+    const [filteredCompanies, setFilteredCompanies] = useState([]);
 
     // get companies
     const getCompanies = async () => {
@@ -22,9 +25,25 @@ const Main = () => {
         getCompanies();
     }, [])
 
+    useEffect(() => {
+        setFilteredCompanies(
+
+            companies.filter(company => {
+                return company.name.toLowerCase().includes(search.toLowerCase())
+            })
+        )
+    }, [search, companies])
+
     return (
         <main>
-            <ListItems companies={companies} />
+            <input
+                className='search-company'
+                type="text"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+                placeholder="Type to search..."
+            />
+            <ListItems companies={filteredCompanies} />
         </main>
     );
 }
