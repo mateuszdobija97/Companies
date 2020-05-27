8980f2d6253bf3ea1803b4d411353f1a7492e173
Mateusz Dobija <mateusz.dobija.rce@gmail.com>
2020-05-27 20:45:08 +0200

    add responsive sort and styles

--- src/components/CompaniesTable.js
+++ src/components/CompaniesTable.js
@@ -3,31 +3,43 @@ import React from 'react';
 const CompaniesTable = ({ companies, sortByNumber, sortByString }) => {
     const companiesItems = companies.map(company => (
         <tr key={company.id}>
-            <td>{company.id}</td>
-            <td>{company.name}</td>
-            <td>{company.city}</td>
-            <td>{company.incomes}</td>
-            <td>{company.avgIncomes.toFixed(2)}</td>
-            <td>{company.lastMonthIncomes}</td>
+            <td data-label='Id:'>{company.id}</td>
+            <td data-label='Name:'>{company.name}</td>
+            <td data-label='City:'>{company.city}</td>
+            <td data-label='Total incomes:'>{company.incomes}</td>
+            <td data-label='Average incomes:'>{company.avgIncomes.toFixed(2)}</td>
+            <td data-label='Last month incomes:'>{company.lastMonthIncomes}</td>
         </tr>
     ))
 
     return (
-        <table className="table-companies">
-            <thead>
-                <tr>
-                    <th onClick={() => sortByNumber('id')}>Id:</th>
-                    <th onClick={() => sortByString('name')}>Name:</th>
-                    <th onClick={() => sortByString('city')}>City:</th>
-                    <th onClick={() => sortByNumber('incomes')}>Total income:</th>
-                    <th onClick={() => sortByNumber('avgIncomes')}>Average income:</th>
-                    <th onClick={() => sortByNumber('lastMonthIncomes')}>Last month income:</th>
-                </tr>
-            </thead>
-            <tbody>
-                {companiesItems}
-            </tbody>
-        </table>
+        <React.Fragment>
+            <div className="responsive-sort" style={{ display: 'none' }}>
+                <ul>
+                    <li onClick={() => sortByNumber('id')}>Id:</li>
+                    <li onClick={() => sortByString('name')}>Name:</li>
+                    <li onClick={() => sortByString('city')}>City:</li>
+                    <li onClick={() => sortByNumber('incomes')}>Total incomes:</li>
+                    <li onClick={() => sortByNumber('avgIncomes')}>Average incomes:</li>
+                    <li onClick={() => sortByNumber('lastMonthIncomes')}>Last month incomes:</li>
+                </ul>
+            </div>
+            <table className="table-companies">
+                <thead>
+                    <tr>
+                        <th onClick={() => sortByNumber('id')}>Id:</th>
+                        <th onClick={() => sortByString('name')}>Name:</th>
+                        <th onClick={() => sortByString('city')}>City:</th>
+                        <th onClick={() => sortByNumber('incomes')}>Total incomes:</th>
+                        <th onClick={() => sortByNumber('avgIncomes')}>Average incomes:</th>
+                        <th onClick={() => sortByNumber('lastMonthIncomes')}>Last month incomes:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {companiesItems}
+                </tbody>
+            </table>
+        </React.Fragment>
     );
 }
 
